3b63638393be9156133475a928289a466e77c4ec
Murat Kucukosman <murat@copypanthers.com>
2013-12-16 12:25:17 +0200

    website download

---
+++ websitedownload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to download website
+
+# download directory
+download_dir="./data/"
+
+echo "Enter the URL for download: "
+read URL
+
+
+httrack ${URL} -K -w -O ${download_dir} -%v --robots=0 -c1 -%e0
